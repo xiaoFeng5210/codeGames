138058e88c308465e060a2dd758f163ac78af302
Eadweard <804875728@qq.com>
2022-03-26 21:47:51 +0800

    项目初始化

---
+++ my-vitesse-app/src/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div>
+      Minesweeper
+
+      <div v-for="y in 10"> 
+        <button v-for="x in 10" w-5 h-5 border>{{x}}</button>
+      </div>
+    </div>
+  </div>
+</template>
