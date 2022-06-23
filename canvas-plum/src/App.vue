503d20c26cef84949d4c22ed8a06b9b0a46d045c
Eadweard <804875728@qq.com>
2022-06-24 01:24:23 +0800

    feat:

--- canvas-plum/src/App.vue
+++ canvas-plum/src/App.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+const el = ref<HTMLCanvasElement>()
+const ctx = $computed(() => el.value!.getContext('2d')!)
+const WIDTH = 600
+const HEIGHT = 600
+function line(x1: number, y1: number, x2: number, y2: number) {
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.stroke()
+}
+
+function init() {
+  ctx.strokeStyle = 'white'
+  line(WIDTH / 2, HEIGHT, WIDTH / 2, HEIGHT / 2)
+}
+
+onMounted(() => {
+  init()
+})
+</script>
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <router-view />
-    <Footer />
-  </main>
+  <canvas ref="el" width="600" height="600" border />
+  <Footer />
 </template>
