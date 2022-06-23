9a8e364d2cd2518e221a0baaf85b521858ccdfc7
Eadweard <804875728@qq.com>
2022-06-24 01:36:05 +0800

    lineTo & line

--- canvas-plum/src/App.vue
+++ canvas-plum/src/App.vue
@@ -3,16 +3,35 @@ const el = ref<HTMLCanvasElement>()
 const ctx = $computed(() => el.value!.getContext('2d')!)
 const WIDTH = 600
 const HEIGHT = 600
-function line(x1: number, y1: number, x2: number, y2: number) {
+
+interface Point {
+  x: number
+  y: number
+}
+interface Line {
+  start: Point
+  length: number
+  theta: number
+}
+function lineTo(p1: Point, p2: Point) {
   ctx.beginPath()
-  ctx.moveTo(x1, y1)
-  ctx.lineTo(x2, y2)
+  ctx.moveTo(p1.x, p1.y)
+  ctx.lineTo(p2.x, p2.y)
   ctx.stroke()
+}
+
+function line(l: Line) {
+  const { start, length, theta } = l
+  const end = { x: start.x + length * Math.cos(theta), y: start.y + length * Math.sin(theta) }
+  lineTo(start, end)
 }
 
 function init() {
   ctx.strokeStyle = 'white'
-  line(WIDTH / 2, HEIGHT, WIDTH / 2, HEIGHT / 2)
+  const startPoint = { x: WIDTH / 2, y: HEIGHT }
+  const endPoint = { x: WIDTH / 2, y: HEIGHT / 2 }
+  lineTo(startPoint, endPoint)
+  lineTo(endPoint, { x: WIDTH / 2 + 50, y: 10 })
 }
 
 onMounted(() => {
