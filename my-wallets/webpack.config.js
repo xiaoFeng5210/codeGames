ea9c3064525e1c3392d4d2b6332e50b493a09fa8
Eadweard <804875728@qq.com>
2022-07-03 12:05:36 +0800

    webpack配置支持ts文件

--- my-wallets/webpack.config.js
+++ my-wallets/webpack.config.js
@@ -1,6 +1,17 @@
 module.exports = {
     entry: './src/main.ts',
     module: {
-
+        rules: [
+            {
+                test: /\.[jt]s?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-typescript', '@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 }
