fad4aaec8e7ddf4e44ef890b89d27178524f8198
Eadweard <804875728@qq.com>
2022-07-03 12:47:03 +0800

    webpack配置完毕

--- my-wallets/webpack.config.js
+++ my-wallets/webpack.config.js
@@ -1,17 +1,32 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/main.ts',
+    mode: 'development',
+    entry: './src/main.js',
+    // resolveLoader: {
+    //     modulesDirectories: [
+    //         './node_modules'
+    //     ]
+    // },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './src'),
+        },
+        compress: true,
+        port: 9000,
+    },
     module: {
-        rules: [
-            {
-                test: /\.[jt]s?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-typescript', '@babel/preset-env']
-                    }
-                }
-            }
-        ]
+        // rules: [
+        //     {
+        //         test: /\.[jt]s?$/,
+        //         exclude: /node_modules/,
+        //         use: {
+        //             loader: 'babel-loader',
+        //             options: {
+        //                 presets: ['@babel/preset-typescript', '@babel/preset-env']
+        //             }
+        //         }
+        //     }
+        // ]
     }
 }
