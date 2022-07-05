00c6c622616b8c0f95f9324db84a26148600a095
Eadweard <804875728@qq.com>
2022-07-06 00:05:11 +0800

    新代码提交

--- my-wallets/src/main.js
+++ my-wallets/src/main.js
@@ -1,4 +1,77 @@
-function test() {
-    console.log('Hello World');
+const allUnitsArr = [...document.querySelectorAll(`.one-unit`)]
+// 单元拖拽时，阻止非添加单元样式切换的事件处理
+let clickable = true;
+let clickTimeId = 0
+
+allUnitsArr.forEach(item => {
+    item.addEventListener("click", handleClick)
+})
+
+function handleClick(event) {
+    if (!clickable) {
+        clickable = true;
+        return
+    }
+    const target = event.currentTarget;
+    if (!target.classList.contains('add-unit')) {
+      const allUnitsArr = [...document.querySelectorAll(`.one-unit`)]
+        allUnitsArr.forEach(item => {
+            item.classList.remove('selected-unit')
+        })
+        target.classList.add('selected-unit');
+    } else {
+        const rootDiv = document.createElement("div");
+        rootDiv.classList.add('one-unit');
+
+        const span = document.createElement("span");
+        span.classList.add("currency")
+        span.innerHTML = "CNY"
+
+        const div = document.createElement("div");
+        const spanInner1 = document.createElement("span");
+        spanInner1.classList.add("num");
+        spanInner1.innerHTML = "75"
+        const spanInner2 = document.createElement("span");
+        spanInner2.classList.add("symbol");
+        spanInner2.innerHTML = "%"
+        const text = document.createTextNode("\n")
+        div.appendChild(spanInner1);
+        div.appendChild(text);
+        div.appendChild(spanInner2);
+
+        rootDiv.appendChild(span)
+        rootDiv.appendChild(div)
+        e.currentTarget.parentNode.insertBefore(rootDiv, e.currentTarget.parentNode.lastElementChild)
+        // TODO:
+        rootDiv.addEventListener("mousedown", handleMouseDown)
+        rootDiv.addEventListener("click", handleClick)
+        rootDiv.addEventListener("mouseup", handleMouseUp)
+    }
 }
-test();
+
+// 容器移动
+
+
+
+
+
+// 目标单元的信息保存
+let target = null
+let targetIndex = 0
+let blockWidth = 0
+// TODO:
+function handleMouseDown(e) {
+    if (e.currentTarget.classList.contains("add-unit")) {
+        return 
+    }
+    clickTImeId = setTimeout(() => {
+        clickable = false;
+    }, 200)
+    const allUnits = [...document.querySelectorAll(`.one-unit`)]
+    allUnits.forEach((item, index) => {
+        if (item === e.currentTarget) {
+            target = item
+            targetIndex = index
+        }
+    })
+}
